b110c6c0f8ce78dbcfe25172bb777c3b5b065b81
SD-PARK <tkdeh129@naver.com>
2023-06-15 12:43:21 +0900

    feat: mysql2 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MysqlModule } from './mysql/mysql.module';
+import { StreamingModule } from './streaming/streaming.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [MysqlModule, StreamingModule,
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/mysql/mysql.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { Streaming } from '../entities/streaming.entity';
+
+@Module({
+  imports: [TypeOrmModule.forRootAsync({
+    useFactory: async (configService: ConfigService) => ({
+      type: 'mysql',
+      host: configService.get('DB_HOST'),
+      port: configService.get('DB_PORT'),
+      username: configService.get('DB_USERNAME'),
+      password: configService.get('DB_PASSWORD'),
+      database: configService.get('DB_NAME'),
+      entities: [Streaming],
+      synchronize: true,
+    }),
+    inject: [ConfigService],
+  })],
+})
+export class MysqlModule {}
