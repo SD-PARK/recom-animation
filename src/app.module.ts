ba780f67baa125d4485abf32b8324d01a0d1eed5
SD-PARK <tkdeh129@naver.com>
2023-06-16 17:51:00 +0900

    feat: AnimationInfoModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MysqlModule } from './mysql/mysql.module';
 import { StreamingModule } from './streaming/streaming.module';
-import { ConfigModule } from '@nestjs/config';
 import { TagModule } from './tag/tag.module';
 import { GenreModule } from './genres/genre.module';
+import { AnimationInfoModule } from './animation-info/animation-info.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MysqlModule, StreamingModule, TagModule, GenreModule,
+  imports: [MysqlModule, StreamingModule, TagModule, GenreModule, AnimationInfoModule,
     ConfigModule.forRoot({
       cache: true,
       isGlobal: true,
