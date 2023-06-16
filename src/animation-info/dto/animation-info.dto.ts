8db14a98a72abf4280c6842daee3ce3ceab8c985
SD-PARK <tkdeh129@naver.com>
2023-06-16 21:52:04 +0900

    feat: animation-info DTO 생성

---
+++ src/animation-info/dto/animation-info.dto.ts
@@ -0,0 +1,56 @@
+import { IsNumber, IsOptional, IsString, Max, MaxLength, Min } from "class-validator";
+
+export class AniamtionInfoDto {
+    // 작품명
+    @IsString()
+    @MaxLength(90)
+    title: string;
+
+    // 포스터 이미지
+    @IsString()
+    @MaxLength(90)
+    image: string;
+
+    // 방영년도
+    @IsNumber()
+    @Min(1900)
+    @Max(2040)
+    aired: number;
+
+    // 감독
+    @IsOptional()
+    @IsString()
+    @MaxLength(20)
+    director: string;
+
+    // 스튜디오
+    @IsString()
+    @MaxLength(20)
+    studio: string;
+
+    // 설명
+    @IsOptional()
+    @IsString()
+    @MaxLength(2000)
+    summary: string;
+
+    // 심의 등급
+    @IsNumber()
+    @Max(19)
+    rated: number;
+
+    // 카테고리
+    @IsOptional()
+    @IsString({ each: true })
+    categories: string[];
+
+    // 태그
+    @IsOptional()
+    @IsString({ each: true })
+    tags: string[];
+
+    // 스트리밍 사이트
+    @IsOptional()
+    @IsString({ each: true })
+    streamings: string[];
+}
