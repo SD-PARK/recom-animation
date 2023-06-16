988ef78c10e06eb8e5e1b395fe017b93b1dafa33
SD-PARK <tkdeh129@naver.com>
2023-06-16 13:25:55 +0900

    refactor: 가독성 개선

--- src/entities/streaming.entity.ts
+++ src/entities/streaming.entity.ts
@@ -2,9 +2,9 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ schema: 'recom-animation', name: 'streamings' })
 export class Streaming {
-    @PrimaryGeneratedColumn({type: 'int', name: 'id'})
+    @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
     id: number;
 
-    @Column('varchar', {name: 'streaming', length: 45})
+    @Column('varchar', { name: 'streaming', length: 45 })
     streaming: string;
 }
