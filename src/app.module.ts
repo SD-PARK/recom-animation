f11dc2a5b44c00ecd4dfddea3bddc11532b7ac4b
SD-PARK <tkdeh129@naver.com>
2023-06-16 21:52:21 +0900

    rename: Genre -> Category 이름 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MysqlModule } from './mysql/mysql.module';
 import { StreamingModule } from './streaming/streaming.module';
+import { CategoryModule } from './category/category.module';
 import { TagModule } from './tag/tag.module';
-import { GenreModule } from './genres/genre.module';
 import { AnimationInfoModule } from './animation-info/animation-info.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmExModule } from './typeorm_ex/typeorm_ex.module';
 
 @Module({
-  imports: [MysqlModule, StreamingModule, TagModule, GenreModule, AnimationInfoModule,
+  imports: [MysqlModule, StreamingModule, CategoryModule, TagModule, AnimationInfoModule,
     ConfigModule.forRoot({
       cache: true,
       isGlobal: true,
-    }),],
+    }),
+    TypeOrmExModule,],
   controllers: [AppController],
   providers: [AppService],
 })
