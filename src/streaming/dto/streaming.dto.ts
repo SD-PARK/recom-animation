7c286900f33f2e43226f935db2c3f0b4c574eee0
SD-PARK <tkdeh129@naver.com>
2023-06-16 13:25:25 +0900

    refactor: streaming 문자열의 최대 길이 제한: 45

--- src/streaming/dto/streaming.dto.ts
+++ src/streaming/dto/streaming.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from 'class-validator';
+import { IsString, MaxLength } from 'class-validator';
 
 export class StreamingDto {
-    @IsString()
+    @IsString({ message: '문자열이어야 합니다.' })
+    @MaxLength(45, { message: '문자열은 최대 45글자까지만 허용됩니다.' })
     streaming: string;
 }
