6350b69421503b9955fac4ebedda6df3a25ec1ca
SD-PARK <tkdeh129@naver.com>
2023-06-16 13:51:18 +0900

    feat: genres 테이블 관련 생성, 조회, 삭제 기능 추가

--- src/genres/genre.controller.ts
+++ src/genres/genre.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
+import { Genre } from 'src/entities/genre.entity';
+import { GenreDto } from './dto/genre.dto';
+import { GenreService } from './genre.service';
 
 @Controller('genres')
-export class GenresController {}
+export class GenreController {
+    constructor(private readonly genreService: GenreService) {}
+
+    @Get()
+    async findAll(): Promise<Genre[]> {
+        return await this.genreService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: number): Promise<Genre> {
+        return await this.genreService.findOne(id);
+    }
+
+    @Post()
+    async create(@Body() genre: GenreDto): Promise<{message: string}> {
+        await this.genreService.create(genre);
+        return { message: 'Success' };
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: number): Promise<{message: string}> {
+        await this.genreService.delete(id);
+        return { message: 'Success' };
+    }
+}
