e00874f002868c560fca26425c77255ffe7d0d18
SD-PARK <tkdeh129@naver.com>
2023-06-16 22:41:40 +0900

    feat: category repository 생성

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
 import { Category } from 'src/db/entities/category.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmExModule } from 'src/typeorm_ex/typeorm_ex.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Category]),
+    TypeOrmExModule.forCustomRepository([Category]),
   ],
   controllers: [CategoryController],
   providers: [CategoryService]
