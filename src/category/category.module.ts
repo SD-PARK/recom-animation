1261913d08f15283fd0fb38cf5fd18bc236a26a2
SD-PARK <tkdeh129@naver.com>
2023-06-16 22:46:52 +0900

    refactor: category repository 생성

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmExModule } from 'src/typeorm_ex/typeorm_ex.module';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
 import { Category } from 'src/db/entities/category.entity';
+import { CategoryRepository } from 'src/db/repository/category.repository';
 
 @Module({
   imports: [
-    TypeOrmExModule.forCustomRepository([Category]),
+    TypeOrmExModule.forCustomRepository([CategoryRepository]),
   ],
   controllers: [CategoryController],
   providers: [CategoryService]
