08183c44aeb1d9457945a44771d0da023fb89597
SD-PARK <tkdeh129@naver.com>
2023-06-16 17:49:43 +0900

    feat: animation-info entity 추가

---
+++ src/animation-info/animation-info.entity.ts
@@ -0,0 +1,28 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('animation_infos')
+export class AnimationInfo extends BaseEntity {
+    @PrimaryGeneratedColumn({ type: 'int', comment: '애니메이션 ID' })
+    id: number;
+
+    @Column({ type: 'varchar', length: 90, nullable: false, comment: '작품명' })
+    title: string;
+
+    @Column({ type: 'varchar', length: 90, nullable: false, comment: '포스터 이미지' })
+    image: string;
+
+    @Column({ type: 'year', nullable: false, comment: '방영년도' })
+    aired: number;
+
+    @Column({ type: 'varchar', length: 20, nullable: true, comment: '감독' })
+    director: string;
+
+    @Column({ type: 'varchar', length: 20, nullable: false, comment: '스튜디오' })
+    studio: string;
+
+    @Column({ type: 'varchar', length: 2000, nullable: true, comment: '설명' })
+    summary: string;
+
+    @Column({ type: 'int', nullable: false, comment: '심의 등급' })
+    rated: number;
+}
