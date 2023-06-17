aa243ef55f96e7bb5048c5ff98d9812b4a8fc43b
SD-PARK <tkdeh129@naver.com>
2023-06-17 19:29:45 +0900

    refactor: 스트리밍 DELETE 요청 시 애니메이션 내의 스트리밍 사이트도 제거

--- src/streaming/streaming.module.ts
+++ src/streaming/streaming.module.ts
@@ -3,10 +3,11 @@ import { StreamingController } from './streaming.controller';
 import { StreamingService } from './streaming.service';
 import { StreamingRepository } from 'src/db/repository/streaming.repository';
 import { TypeOrmExModule } from 'src/typeorm_ex/typeorm_ex.module';
+import { AnimationStreamingRepository } from 'src/db/repository/animation-streaming.repository copy';
 
 @Module({
   imports: [
-    TypeOrmExModule.forCustomRepository([StreamingRepository]),
+    TypeOrmExModule.forCustomRepository([StreamingRepository, AnimationStreamingRepository]),
   ],
   controllers: [StreamingController],
   providers: [StreamingService]
--- src/streaming/streaming.service.ts
+++ src/streaming/streaming.service.ts
@@ -1,12 +1,15 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
-import { EntityNotFoundError } from 'typeorm';
 import { Streaming } from 'src/db/entities/streaming.entity';
 import { StreamingDto } from './dto/streaming.dto';
 import { StreamingRepository } from 'src/db/repository/streaming.repository';
+import { AnimationStreamingRepository } from 'src/db/repository/animation-streaming.repository copy';
 
 @Injectable()
 export class StreamingService {
-    constructor(private readonly streamingRepository: StreamingRepository) {}
+    constructor(
+        private readonly streamingRepository: StreamingRepository,
+        private readonly animationStreamingRepository: AnimationStreamingRepository,
+    ) {}
 
     /**
      * 스트리밍 레코드를 생성합니다.
@@ -32,10 +35,11 @@ export class StreamingService {
      */
     async findOne(streaming: string): Promise<Streaming> {
         try {
-            return await this.streamingRepository.findStreaming({ where: { streaming: streaming } });
+            const result: Streaming = await this.streamingRepository.findStreaming(streaming);
+            if (!result) { throw new NotFoundException(`일치하는 데이터를 찾을 수 없습니다: ${streaming}`); }
+            return result;
         } catch(err) {
-            if (err instanceof EntityNotFoundError) { throw new NotFoundException(`일치하는 데이터를 찾을 수 없습니다: ${streaming}`); }
-            else { console.error('오류가 발생했습니다:', err.message); throw err; }
+            console.error('오류가 발생했습니다:', err.message); throw err;
         }
     }
 
@@ -54,8 +58,9 @@ export class StreamingService {
      * @thrwos Error - 삭제 과정 중 다른 오류가 발생한 경우
      */
     async delete(streaming: string): Promise<void> {
-        const findStreaming = await this.findOne(streaming);
+        const findStreaming: Streaming = await this.findOne(streaming);
         try {
+            await this.animationStreamingRepository.deleteBystreamingId(findStreaming.id);
             await this.streamingRepository.deleteStreaming(findStreaming.id);
         } catch(err) {
             console.error('오류가 발생했습니다:', err.message);
