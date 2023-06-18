f68973e0b618d0614bd76c156e8f966f4bdacd3b
SD-PARK <tkdeh129@naver.com>
2023-06-18 16:29:33 +0900

    refactor: Category와 Tag 분류 통합 => Tag

--- src/animation-info/animation-info.service.spec.ts
+++ src/animation-info/animation-info.service.spec.ts
@@ -1,10 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AnimationInfoService } from './animation-info.service';
-import { AnimationCategoryRepository } from 'src/db/repository/animation-category.repository';
 import { AnimationInfoRepository } from 'src/db/repository/animation-info.repository';
 import { AnimationStreamingRepository } from 'src/db/repository/animation-streaming.repository copy';
 import { AnimationTagRepository } from 'src/db/repository/animation-tag.repository';
-import { CategoryRepository } from 'src/db/repository/category.repository';
 import { StreamingRepository } from 'src/db/repository/streaming.repository';
 import { TagRepository } from 'src/db/repository/tag.repository';
 
@@ -15,10 +13,8 @@ describe('AnimationInfoService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AnimationInfoService,
                   AnimationInfoRepository,
-                  CategoryRepository,
                   TagRepository,
                   StreamingRepository,
-                  AnimationCategoryRepository,
                   AnimationTagRepository,
                   AnimationStreamingRepository],
     }).compile();
